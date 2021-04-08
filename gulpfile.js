4892a3ed67e73749e5fb760d64c33b367f165ecb
AndrewVakaryuk <andreyvakaryuk@gmail.com>
2021-04-08 15:52:25 +0300

    little cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify-es').default;
 
 // create task
 gulp.task('css', async function () {
@@ -9,6 +11,16 @@ gulp.task('css', async function () {
     .pipe(minifyCSS())
     .pipe(concat('main.min.css'))
     .pipe(gulp.dest('assets/css'));
+});
+
+//Файл збирається і підгружається на сторінку, але все одно не працює
+gulp.task("minify", function () {
+  return gulp.src("src/js/*.js")
+    .pipe(rename("main.min.js"))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write()) 
+    .pipe(gulp.dest("assets/js"));
 });
 
 gulp.task('default', gulp.series('css'));
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-@include 'accordions';
-@include 'bootstrap.bundle.min';
-@include 'custom';
-@include 'isotope';
-@include 'owl';
-@include 'slick';
